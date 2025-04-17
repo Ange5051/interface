0290ad10f1ce25ec3602f9c8a79bba6cb89b5a5c
Ange5051 <147865401+Ange5051@users.noreply.github.com>
2025-04-17 10:59:50 +0300

    Add files via upload

---
+++ ConsoleApp11/ConsoleApp11/IInterface.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    interface IInterface
+    {
+        double MyGetPI();
+        int MyGetInt();
+        int MySquare(int x);
+        double MySqrt(double x);
+
+    }
+}
---
+++ ConsoleApp11/ConsoleApp11/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+            Console.WriteLine($"Метод, возвращающий число PI - {myClass.MyGetPI()}");
+            Console.WriteLine($"Метод, возвращающий число типа int - {myClass.MyGetInt()}");
+            Console.WriteLine($"Метод, возвращающий x в квадрате - {myClass.MySquare(3)}");
+            Console.WriteLine($"Метод, возвращающий корень квадратный из x - {myClass.MySqrt(9)}");
+            Console.ReadKey();
+        }
+    }
+}
